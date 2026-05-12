872e43c2bbe4ae03e1780be4b5a9f699c2bb7564
D1nma <hedin.maxence2@gmail.com>
2026-05-13 00:52:58 +0200

    chore: remove test files

--- test_loop.sh
+++
@@ -1,9 +0,0 @@
-tmp_dirs="test_tmp_dirs"
-printf '123\t/path/to/dir\0' > "$tmp_dirs"
-printf '456\t/another/dir\0' >> "$tmp_dirs"
-out_file="test_out"
-> "$out_file"
-while IFS=$'\t' read -r -d '' val path; do
-  printf '%s\td:%s\0' "$val" "$path" >> "$out_file"
-done < "$tmp_dirs"
-cat "$out_file" | od -c
